f42e724a8b54b60023155ae4a9341196e25a67eb
thomas.opdebeeck <thomas.opdebeeck@adflumine.com>
2022-12-11 17:59:02 +0100

    more compact syntax

--- src/server/routes/root/root.router.ts
+++ src/server/routes/root/root.router.ts
@@ -3,8 +3,5 @@ import {apiRouter} from "../api";
 
 export const router = express.Router();
 
-router.get('/',(req, res) => {
-    res.send('Express + TypeScript Server');
-});
-
+router.get('/',(req, res) => res.send('Express + TypeScript Server'));
 router.use('/api', apiRouter)
