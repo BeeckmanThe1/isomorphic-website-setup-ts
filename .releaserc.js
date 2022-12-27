9ce232ba4738f05f3da9fca8e53f6afabb24c485
thomas.opdebeeck <thomas.opdebeeck@adflumine.com>
2022-12-27 18:27:25 +0100

    feat: try to generate changelog

--- .releaserc.js
+++ .releaserc.js
@@ -1,4 +1,20 @@
 module.exports = {
     tagFormat: 'v${version}',
-    branches: ['main']
+    branches: ['main'],
+    "plugins": [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        [
+            "@semantic-release/changelog",
+            {
+                "changelogFile": "docs/CHANGELOG.md"
+            }
+        ],
+        [
+            "@semantic-release/git",
+            {
+                "assets": ["docs/CHANGELOG.md"]
+            }
+        ]
+    ]
 }
